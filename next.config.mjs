74a3fcee90246f8aa8129212e345928caf337124
Zach Waterfield <zlwaterfield@gmail.com>
2024-04-22 21:17:45 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['logo.clearbit.com'],
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
